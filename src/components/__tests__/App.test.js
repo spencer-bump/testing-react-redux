2a2c4c7533922007ac1ee82ee4639a8c6f8c53b0
Spencer Eldred <eldredspencer@gmail.com>
2019-08-24 09:05:58 -1000

    first commit - first enzyme test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+
+
+it('shows a comment box',() => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
