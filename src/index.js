f9ed78b86cb42e881478e6abb14539e360e60a03
Spencer Eldred <eldredspencer@gmail.com>
2019-08-27 08:22:24 -1000

    Add react-router-dom and wrap App with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Root from './Root.jsx';
 import App from './components/App.jsx';
 
 ReactDom.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>,
   document.querySelector('#root')
 );
